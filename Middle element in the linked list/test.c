1480c8eee918216501435257c3ab7ee5405bd41a
404S-retr0 <77523312+404S-retr0@users.noreply.github.com>
2021-04-01 18:00:14 +0530

    Create test.c

---
+++ Middle element in the linked list/test.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void printMiddle(struct Node *head)
+{
+    struct Node *slow_ptr = head;
+    struct Node *fast_ptr = head;
+    if(head!=NULL)
+    {
+        while (fast_ptr != NULL && fast_ptr->next != NULL)
+        {
+            fast_ptr = fast_ptr->next->next;
+            slow_ptr = slow_ptr->next;
+        }
+        printf("%d", slow_ptr->data);
+    }
+}
+
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node =
+        (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+void printList(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+int main()
+{
+    struct Node* head = NULL;
+    int n;
+    scanf("%d", &n);
+ 
+    for (int i=0; i<n; i++)
+    {
+        int p=0;
+        scanf("%d", &p);
+        push(&head, p);
+    }
+    printMiddle(head);
+    return 0;
+}
